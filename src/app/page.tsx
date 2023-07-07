9dd38ca23e9da8eeb5a3da11b3419ddc76860ced
karengomezs <karengomezsoto1@gmail.com>
2023-07-06 19:38:29 -0500

    font colors and disposition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
 
   return (
     <main className="bg-slate-950 h-screen flex flex-col gap-5 px-6 pt-14 w-full md:px-56">
-      <p className="font-extrabold text-4xl text-slate-200">
+      <p className="font-extrabold text-4xl text-white">
         What's up, {user?.firstName}!
       </p>
       <p className="text-orange-500">Today's {today}</p>
@@ -124,11 +124,14 @@ export default function Home() {
         </Form.Submit>
       </Form.Root>
 
-      <div className="flex flex-wrap gap-3 my-10 p-3 h-full bg-slate-900 rounded-md">
+      <div className="flex flex-wrap gap-3 my-10 p-3  bg-slate-900 rounded-md">
         {tasksList.map((todo) => {
           return (
-            <div className="flex-1 rounded-md h-32 bg-orange-500" key={todo.id}>
-              <p className={`${todo.status === "done" ? "line-through" : ""}`}>
+            <div
+              className="flex-1 p-2 font-bold text-lg text-white rounded-md bg-orange-500"
+              key={todo.id}
+            >
+              <p className={`${todo.status === "done" ? "line-through" : ""} `}>
                 {todo.task}
               </p>
               <div className="flex gap-4">
@@ -139,7 +142,20 @@ export default function Home() {
                     setValue("status", todo.status);
                   }}
                 >
-                  ✏️
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-slate-900"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                    />
+                  </svg>
                 </button>
                 <input
                   type="checkbox"
